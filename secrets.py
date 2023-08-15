20b57ca577666f1bcc695a5766478d12c244681e
Nrico <etrujillo@gmail.com>
2023-08-15 13:16:34 -0600

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,7 @@
+secrets = {
+    'ssid' : 'YOURNETID',
+    'password' : 'YOURNETWORKPASSWORD',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'aio_username' : 'ADAFRUITIOUSERNAME',
+    'aio_key' : 'ADAFRUITIOKEY',
+}
